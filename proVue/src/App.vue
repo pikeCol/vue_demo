974556a5586992df74a1e3ba67de16a0b90c7d5b
pikeCol <m13621613594@163.com>
2016-12-12 14:46:34 +0800

    vue demo

---
+++ proVue/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <v-header></v-header>
+    <div class="tab">
+      <div class="tab-item">商品</div>
+      <div class="tab-item">评论</div>
+      <div class="tab-item">商家</div>
+    </div>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  import header from './components/header/header';
+  export default {
+    components: {
+      'v-header': header
+    }
+  };
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  .tab 
+    display: flex
+    width: 100%
+    height: 40px
+    line-height: 40px
+    .tab-item
+      flex: 1
+      text-align: center
+</style>
